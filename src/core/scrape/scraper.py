332610c06c1e39e777108bb5e45cf19238cf9dee
cctong-castiel <castieltong@gmail.com>
2025-11-08 14:17:50 +0800

    branch for running prefect locally

--- src/core/scrape/scraper.py
+++ src/core/scrape/scraper.py
@@ -4,9 +4,9 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 import html2text
 import json
-# from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
 from prefect import task
-# from src.core.scrape.base import SeleniumScraperBase
+from src.core.scrape.base import SeleniumScraperBase
 from src.core.utils import remove_html_tags, extract_only_alphanumeric
 from src.config.settings import (
     IRD_PDF_METADATA_URL, IRD_CASE_URL, IRD_ADVANCE_CASE_URL,
@@ -14,93 +14,93 @@ from src.config.settings import (
 )
 
 
-# class IrdTableScraper(SeleniumScraperBase):
-#     """
-#     A class to scrape the IRD case table from the specified URL and save the results to a JSON file.
-#     """
+class IrdTableScraper(SeleniumScraperBase):
+    """
+    A class to scrape the IRD case table from the specified URL and save the results to a JSON file.
+    """
 
-#     def __init__(self, url: str = IRD_ADVANCE_CASE_URL):
-#         super().__init__(url=url)
-#         self.url = url
+    def __init__(self, url: str = IRD_ADVANCE_CASE_URL):
+        super().__init__(url=url)
+        self.url = url
 
-#     def fetch_page(self):
-#         """
-#         A method to fetch and parse the IRD case table from the webpage.
-#         """
+    def fetch_page(self):
+        """
+        A method to fetch and parse the IRD case table from the webpage.
+        """
 
-#         # First part - extract the headers part
-#         gov_page_title = self.driver.find_element(By.CSS_SELECTOR, '#content-area > section > div.navi > a:nth-child(3)').get_attribute('innerHTML')
-#         section_name = self.driver.find_element(By.CSS_SELECTOR, '#content > div.content-div > div.introduction > p:nth-child(6) > strong').get_attribute('innerHTML')
-#         table_loc = self.driver.find_element(By.CSS_SELECTOR, '#content > div.content-div > div:nth-child(3) > div.toggle_div > div > div:nth-child(2)').get_attribute('innerHTML')
+        # First part - extract the headers part
+        gov_page_title = self.driver.find_element(By.CSS_SELECTOR, '#content-area > section > div.navi > a:nth-child(3)').get_attribute('innerHTML')
+        section_name = self.driver.find_element(By.CSS_SELECTOR, '#content > div.content-div > div.introduction > p:nth-child(6) > strong').get_attribute('innerHTML')
+        table_loc = self.driver.find_element(By.CSS_SELECTOR, '#content > div.content-div > div:nth-child(3) > div.toggle_div > div > div:nth-child(2)').get_attribute('innerHTML')
         
-#         # Second part - extract table data
-#         table = self.driver.find_element(By.CSS_SELECTOR, 'table.border_table')
-#         rows = table.find_elements(By.TAG_NAME, 'tr')[1:]  # skip header row
+        # Second part - extract table data
+        table = self.driver.find_element(By.CSS_SELECTOR, 'table.border_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')[1:]  # skip header row
 
-#         results = []
-#         for row in rows:
-#             cols = row.find_elements(By.TAG_NAME, 'td')
-#             case_no = cols[0].find_element(By.TAG_NAME, 'a').get_attribute('innerHTML') if cols[0].find_elements(By.TAG_NAME, 'a') else None
-#             case_link = cols[0].find_element(By.TAG_NAME, 'a').get_attribute('href') if cols[0].find_elements(By.TAG_NAME, 'a') else None
-#             provision = cols[1].get_attribute('innerHTML') if cols[1] else None
-#             index_items = [li.get_attribute('innerHTML') for li in cols[2].find_elements(By.TAG_NAME, 'li')]
-#             results.append({
-#                 'gov_page_title': gov_page_title,
-#                 'section_name': section_name,
-#                 'table_loc': table_loc,
-#                 'case_no': case_no,
-#                 'case_link': case_link,
-#                 'provision': provision,
-#                 'index': index_items,
-#             })
+        results = []
+        for row in rows:
+            cols = row.find_elements(By.TAG_NAME, 'td')
+            case_no = cols[0].find_element(By.TAG_NAME, 'a').get_attribute('innerHTML') if cols[0].find_elements(By.TAG_NAME, 'a') else None
+            case_link = cols[0].find_element(By.TAG_NAME, 'a').get_attribute('href') if cols[0].find_elements(By.TAG_NAME, 'a') else None
+            provision = cols[1].get_attribute('innerHTML') if cols[1] else None
+            index_items = [li.get_attribute('innerHTML') for li in cols[2].find_elements(By.TAG_NAME, 'li')]
+            results.append({
+                'gov_page_title': gov_page_title,
+                'section_name': section_name,
+                'table_loc': table_loc,
+                'case_no': case_no,
+                'case_link': case_link,
+                'provision': provision,
+                'index': index_items,
+            })
 
-#         with open(os.path.join(IRD_DATA_DIR, 'ird_results.json'), 'w', encoding='utf-8') as f:
-#             json.dump(results, f, ensure_ascii=False, indent=4)
+        with open(os.path.join(IRD_DATA_DIR, 'ird_results.json'), 'w', encoding='utf-8') as f:
+            json.dump(results, f, ensure_ascii=False, indent=4)
 
 
-# class IrdPdfMetadataScraper(SeleniumScraperBase):
-#     """
-#     A class to scrape the IRD PDF metadata from the specified URL and save the results to a JSON file.
-#     """
+class IrdPdfMetadataScraper(SeleniumScraperBase):
+    """
+    A class to scrape the IRD PDF metadata from the specified URL and save the results to a JSON file.
+    """
 
-#     def __init__(self, url: str = IRD_PDF_METADATA_URL):
-#         super().__init__(url=url)
-#         self.url = url
+    def __init__(self, url: str = IRD_PDF_METADATA_URL):
+        super().__init__(url=url)
+        self.url = url
 
-#     def fetch_page(self):
-#         """
-#         A method to fetch and parse the IRD PDF metadata table from the webpage.
-#         """
+    def fetch_page(self):
+        """
+        A method to fetch and parse the IRD PDF metadata table from the webpage.
+        """
 
-#         # First part - extract the headers part
-#         gov_page_title = self.driver.find_element(By.CSS_SELECTOR, '#content-area > section > div.navi > a:nth-child(3)').get_attribute('innerHTML')
-#         section_name = self.driver.find_element(By.CSS_SELECTOR, '#content > div.content-title-div > h1').get_attribute('innerHTML')
+        # First part - extract the headers part
+        gov_page_title = self.driver.find_element(By.CSS_SELECTOR, '#content-area > section > div.navi > a:nth-child(3)').get_attribute('innerHTML')
+        section_name = self.driver.find_element(By.CSS_SELECTOR, '#content > div.content-title-div > h1').get_attribute('innerHTML')
         
-#         # Second part - extract table data
-#         table = self.driver.find_element(By.CSS_SELECTOR, 'table.border_table')
-#         rows = table.find_elements(By.TAG_NAME, 'tr')[1:]  # skip header row
+        # Second part - extract table data
+        table = self.driver.find_element(By.CSS_SELECTOR, 'table.border_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')[1:]  # skip header row
 
-#         results = []
-#         for row in rows:
-#             cols = row.find_elements(By.TAG_NAME, 'td')
+        results = []
+        for row in rows:
+            cols = row.find_elements(By.TAG_NAME, 'td')
 
-#             pdf_no = cols[0].find_element(By.TAG_NAME, 'a').get_attribute('innerHTML') if cols[0].find_elements(By.TAG_NAME, 'a') else None
-#             pdf_link = cols[0].find_element(By.TAG_NAME, 'a').get_attribute('href') if cols[0].find_elements(By.TAG_NAME, 'a') else None
-#             pdf_notes = cols[1].find_element(By.TAG_NAME, 'a').get_attribute('innerHTML') if cols[1] else None
-#             pdf_notes = remove_html_tags(text=pdf_notes) if pdf_notes else None
-#             pdf_date = cols[2].get_attribute('innerHTML') if cols[2] else None
-#             results.append({
-#                 'gov_page_title': gov_page_title,
-#                 'section_name': section_name,
-#                 'table_loc': section_name,
-#                 'pdf_no': extract_only_alphanumeric(text=pdf_no),
-#                 'pdf_link': pdf_link,
-#                 'pdf_notes': pdf_notes,
-#                 'index': pdf_date,
-#             })
+            pdf_no = cols[0].find_element(By.TAG_NAME, 'a').get_attribute('innerHTML') if cols[0].find_elements(By.TAG_NAME, 'a') else None
+            pdf_link = cols[0].find_element(By.TAG_NAME, 'a').get_attribute('href') if cols[0].find_elements(By.TAG_NAME, 'a') else None
+            pdf_notes = cols[1].find_element(By.TAG_NAME, 'a').get_attribute('innerHTML') if cols[1] else None
+            pdf_notes = remove_html_tags(text=pdf_notes) if pdf_notes else None
+            pdf_date = cols[2].get_attribute('innerHTML') if cols[2] else None
+            results.append({
+                'gov_page_title': gov_page_title,
+                'section_name': section_name,
+                'table_loc': section_name,
+                'pdf_no': extract_only_alphanumeric(text=pdf_no),
+                'pdf_link': pdf_link,
+                'pdf_notes': pdf_notes,
+                'index': pdf_date,
+            })
 
-#         with open(os.path.join(IRD_DATA_DIR, 'ird_pdf_results.json'), 'w', encoding='utf-8') as f:
-#             json.dump(results, f, ensure_ascii=False, indent=4)
+        with open(os.path.join(IRD_DATA_DIR, 'ird_pdf_results.json'), 'w', encoding='utf-8') as f:
+            json.dump(results, f, ensure_ascii=False, indent=4)
 
 
 class IrdCaseContentSpider(scrapy.Spider):
@@ -191,13 +191,13 @@ def scrape_step():
     """
 
     # init
-    # ird_table_scraper = IrdTableScraper()
-    # ird_pdf_metadata_scraper = IrdPdfMetadataScraper()
+    ird_table_scraper = IrdTableScraper()
+    ird_pdf_metadata_scraper = IrdPdfMetadataScraper()
 
     # run
-    # ird_table_scraper.scrape
+    ird_table_scraper.scrape
     run_spider(spider=IrdCaseContentSpider)
 
-    # ird_pdf_metadata_scraper.scrape
+    ird_pdf_metadata_scraper.scrape
     # download_one_pdf(destination_directory=IRD_PDF_DIR)
     # download_pdfs(destination_directory=IRD_PDF_DIR)
